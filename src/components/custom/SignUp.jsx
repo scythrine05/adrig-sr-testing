d614346f5fe098029c9527fb54eb356bda0f2ad3
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-11-26 22:24:47 +0530

    bug resolution

--- src/components/custom/SignUp.jsx
+++ src/components/custom/SignUp.jsx
@@ -353,6 +353,8 @@ export function SignUp() {
                     <select
                       name="department"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                      value={formValues.department}
+                      onChange={handleChange}
                       required
                     >
                       <option value="">Select a department</option>
