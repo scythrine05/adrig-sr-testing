4e9956f060eadb5c52ae673f1c1d93a2a429a458
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-12-07 15:55:08 +0530

    deleted some files

--- prisma/migrations/20241207101016_modification_to_manager_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Manager" ALTER COLUMN "name" SET DEFAULT '';
-
--- AlterTable
-ALTER TABLE "Requests" ALTER COLUMN "requestId" SET DEFAULT (TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDD') || '-' || LPAD(NEXTVAL('request_id_seq')::TEXT, 6, '0'))::TEXT;
-
--- AlterTable
-ALTER TABLE "StagingRequests" ALTER COLUMN "requestId" SET DEFAULT (TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDD') || '-' || LPAD(NEXTVAL('request_id_seq')::TEXT, 6, '0'))::TEXT;
