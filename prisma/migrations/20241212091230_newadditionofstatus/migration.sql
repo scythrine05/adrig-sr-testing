60d9096f191a0a09656f595831b286ca98d0beaf
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-12-15 18:05:37 +0530

    db adjustment

---
+++ prisma/migrations/20241212091230_newadditionofstatus/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Requests" ALTER COLUMN "requestId" SET DEFAULT (TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDD') || '-' || LPAD(NEXTVAL('request_id_seq')::TEXT, 6, '0'))::TEXT;
+
+-- AlterTable
+ALTER TABLE "StagingRequests" ADD COLUMN     "status" TEXT,
+ALTER COLUMN "requestId" SET DEFAULT (TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDD') || '-' || LPAD(NEXTVAL('request_id_seq')::TEXT, 6, '0'))::TEXT;
