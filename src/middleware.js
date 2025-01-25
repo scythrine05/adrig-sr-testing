915a1f30c1f0fb1d35aaffceb253e332f84e237d
Krithik Srinivas <123297016+Krithik-sri@users.noreply.github.com>
2025-01-26 01:30:11 +0530

    updated middleware to support prod

--- src/middleware.js
+++ src/middleware.js
@@ -22,8 +22,8 @@ export async function middleware(request) {
   const adminRoutes = ["/ad/ad-home", "/ad/ad-form", "/ad/ad-optimised-table"];
   const userRoutes = ["/"];
 
-  //__Secure-
-  if (!request.cookies.get("next-auth.session-token")) {
+
+  if (!request.cookies.get("__Secure-next-auth.session-token")) {
     const url = request.nextUrl.clone();
     url.pathname = "/signin";
     return NextResponse.redirect(url);
