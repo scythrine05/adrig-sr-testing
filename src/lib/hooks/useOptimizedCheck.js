8d269cab250e97d4ab3f8787c869d7fa8788293a
Krithik Srinivas <123297016+Krithik-sri@users.noreply.github.com>
2025-01-26 10:09:49 +0530

    fixed an impor error

--- src/lib/hooks/useOptimizedCheck.js
+++ src/lib/hooks/useOptimizedCheck.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { currentOptimizedValue } from "../../app/actions/user";
-import { getUser } from "../../app/actions/user";
+import { getUser } from "../../lib/auth";
 
 const useOptimizedCheck = () => {
   const [optimizedCheck, setOptimizedCheck] = useState(null);
